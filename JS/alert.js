db99d582700142fa72ad2b4b825602c47a870b56
Seryeong <srlimvp@gmail.com>
2023-04-11 04:06:03 +0900

    first commit

---
+++ JS/alert.js
@@ -0,0 +1,40 @@
+// let name = 'John';
+// admin = name;
+// alert(admin)
+
+// let ourPlant
+// let curUserName
+
+// let apples = "2";
+// let oranges = "5";
+// alert(apples + oranges);
+// alert(+apples + +oranges);
+
+// let a = "1";
+// let b = "2";
+// alert(a+b);
+
+let result;
+if (a + b < 4) {
+    result = '미만';
+} else {
+    result = '이상';
+}
+// let result = (a + b < 4) ? '미만' : '이상';
+
+let message;
+
+if (login == '직원') {
+  message = '안녕하세요.';
+} else if (login == '임원') {
+  message = '환영합니다.';
+} else if (login == '') {
+  message = '로그인이 필요합니다.';
+} else {
+  message = '';
+}
+
+// let message = (login == '직원') ? '안녕하세요' :
+// (login == '임원') ? '환영합니다.' :
+// (login == '') ? '로그인이 필요합니다.' :
+// '';
